46ddcee8bae5e648b4e4900fab467ce76336bdc8
Samridhi Gupta <guptasamridhi98@gmail.com>
2026-08-09 17:48:37 +0530

    added drop box in mid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -461,6 +461,7 @@ export default function App() {
           />
 
           <PdfViewer
+          onFileSelected={handleFileSelected}
             ref={pdfViewerRef}
             hidden={mode !== 'original'}
             hasContent={hasDoc}
